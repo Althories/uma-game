af85083ae21ba7f21515f6c63b9de7b08a09eec4
Soampbar <Althories.bar@gmail.com>
2025-08-12 12:44:50 -0500

    - minor fixes
    - update in daiwa sprite
    - changed player script to prevent accidentally jumping when ending dialogue with SPACE

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,41 +3,52 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
-var can_interact = true
+var can_interact = true		#for re-initiating dialogue in process
+var can_move = true			#for player input movement physics process
 
 @onready var player_sprite: Sprite2D = $PlayerSprite
 
 func _process(_delta: float) -> void:
+	#Allow dialogue interaction
 	if Input.is_action_just_pressed("interact") and can_interact == true:
 		DialogueManager.show_dialogue_balloon(load("res://Dialogue/test_dialogue.dialogue"), "start")
 		Globalvars.dialogue_ended = false
 		can_interact = false
-	elif Globalvars.dialogue_ended == true:
+		can_move = false
+	elif Globalvars.dialogue_ended:
 		can_interact = true
 		
 		
 	#Faces Player Sprite in appropriate direction based on movement direction
-	if Input.is_action_just_pressed("move_left"):
-		player_sprite.flip_h = true
-	elif Input.is_action_just_pressed("move_right"):
-		player_sprite.flip_h = false
+	if Globalvars.dialogue_ended:
+		if Input.is_action_just_pressed("move_left"):
+			player_sprite.flip_h = true
+		elif Input.is_action_just_pressed("move_right"):
+			player_sprite.flip_h = false
 		
 		
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#Player movement block - if player is not in dialogue
+	if can_move:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle jump.
+		if Input.is_action_just_pressed("move_jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		var direction := Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	#put at bottom to prevent accidental jump upon dialogue end with SPACE
+	if Globalvars.dialogue_ended:
+		@warning_ignore("standalone_expression")	#it does have an effect, actually :nerd:
+		can_move == true
 
-	move_and_slide()
+		move_and_slide()
